592227762f4cd03557310501b982a8d348d64359
오석호 <dlscjs8646@naver.com>
2022-07-20 14:55:38 +0900

    :bug: eslint 불필요한 설정 제거

--- client/src/components/Home/LeftSide.jsx
+++ client/src/components/Home/LeftSide.jsx
@@ -1,24 +1,24 @@
 /** @jsxImportSource @emotion/react */
-import { css } from "@emotion/react";
-import MovingLogo from "../../images/movingLogo.gif";
+import { css } from '@emotion/react';
+import MovingLogo from '../../images/movingLogo.gif';
 
-const LeftSide = () => {
+function LeftSide() {
   return (
     <div css={totalContainer}>
-        <img src={MovingLogo} alt="움직이는 기본로고" />
+      <img src={MovingLogo} alt="움직이는 기본로고" />
     </div>
-  )
+  );
 }
 
 const totalContainer = css`
-    width: 40%;
-    height: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    img {
-        width: 400px;
-    }
-`
+  width: 40%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  img {
+    width: 400px;
+  }
+`;
 
-export default LeftSide
+export default LeftSide;
--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -1,28 +1,32 @@
 /** @jsxImportSource @emotion/react */
-import { css } from "@emotion/react";
+import { css } from '@emotion/react';
 import LeftSide from '../components/Home/LeftSide';
 import RightSide from '../components/Home/RightSide';
 
-const Home = () => {
+function Home() {
   return (
     <div css={totalContainer}>
-        <LeftSide />
-        <RightSide />
+      <LeftSide />
+      <RightSide />
     </div>
-  )
+  );
 }
 
 const totalContainer = css`
-    position: absolute;
-    width: 90vw;
-    height: 90vh;
-    left: 5vw;
-    top: 5vh;
+  position: absolute;
+  width: 90vw;
+  height: 90vh;
+  left: 5vw;
+  top: 5vh;
 
-    background: linear-gradient(185.82deg, rgba(127, 192, 228, 0.7) 0%, rgba(94, 103, 190, 0.357) 95.83%);
-    border-radius: 20px;
-    display: flex;
-    flex-direction: row;
+  background: linear-gradient(
+    185.82deg,
+    rgba(127, 192, 228, 0.7) 0%,
+    rgba(94, 103, 190, 0.357) 95.83%
+  );
+  border-radius: 20px;
+  display: flex;
+  flex-direction: row;
 `;
 
-export default Home
+export default Home;
