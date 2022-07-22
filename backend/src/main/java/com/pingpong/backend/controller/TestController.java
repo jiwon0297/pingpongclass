b51d4fad60b3a97a438341d268321dd2244b2f12
DESKTOP-KVCQHCD\SSAFY <pyjin805@naver.com>
2022-07-22 10:48:38 +0900

    :bug: 수정을 위한 커밋

--- backend/src/main/java/com/pingpong/backend/controller/TestController.java
+++ backend/src/main/java/com/pingpong/backend/controller/TestController.java
@@ -1,4 +1,4 @@
-package com.pingpong.backend.api.controller;
+package com.pingpong.backend.controller;
 
 import com.pingpong.backend.api.entity.User;
 import com.pingpong.backend.api.repository.UserRepository;
--- backend/src/main/java/com/pingpong/backend/controller/UserController.java
+++ backend/src/main/java/com/pingpong/backend/controller/UserController.java
@@ -1,4 +1,4 @@
-package com.pingpong.backend.api.controller;
+package com.pingpong.backend.controller;
 
 import com.pingpong.backend.api.entity.User;
 import com.pingpong.backend.api.repository.UserRepository;
@@ -8,7 +8,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
