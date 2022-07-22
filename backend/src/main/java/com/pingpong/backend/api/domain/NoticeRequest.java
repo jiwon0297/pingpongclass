3df8581956e5748ebc3cfcd857bfe5c7dcaefac7
Ssoco <dlscjs8646@naver.com>
2022-07-22 18:13:16 +0900

    Revert "[Feature/T2-8, T2-9, T2-10, T2-11] :sparkles: 공지사항 리스트 조회/수정/삭제/작성 완료"
    This reverts merge request !16

--- backend/src/main/java/com/pingpong/backend/api/domain/NoticeRequest.java
+++ backend/src/main/java/com/pingpong/backend/api/domain/NoticeRequest.java
@@ -3,9 +3,11 @@ package com.pingpong.backend.api.domain;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class NoticeRequest {
+
     private int classId;
     private int teacherId;
     private String title;
--- backend/src/main/java/com/pingpong/backend/api/service/NoticeService.java
+++ backend/src/main/java/com/pingpong/backend/api/service/NoticeService.java
@@ -11,9 +11,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -24,82 +22,27 @@ public class NoticeService {
     private final TeacherRepository teacherRepository;
     private final ClassRepository classRepository;
 
-    //공지사항 작성
+    //게시글 작성
     @Transactional
-    public int save(final NoticeRequest request){
-        ClassEntity classEntity = classRepository.getOne(request.getClassId());
-        TeacherEntity teacherEntity = teacherRepository.getOne(request.getTeacherId());
+    public int save(final NoticeRequest params){
+        ClassEntity classEntity = classRepository.getOne(params.getClassId());
+        TeacherEntity teacherEntity = teacherRepository.getOne(params.getTeacherId());
         NoticeEntity noticeEntity = NoticeEntity.builder()
                 .classEntity(classEntity)
                 .teacherEntity(teacherEntity)
-                .title(request.getTitle())
-                .content(request.getContent())
+                .title(params.getTitle())
+                .content(params.getContent())
                 .build();
         NoticeEntity entity = noticeRepository.save(noticeEntity);
         return entity.getNoticeId();
     }
 
-    //공지사항 리스트 조회 및 검색(선생님)
-    public List<NoticeResponse> findTeacher(final int teacherId, final int classId, final String titleSearch){
+    //게시글 리스트 조회
+    public List<NoticeResponse> findAll(){
         Sort sort = Sort.by(Sort.Direction.DESC, "noticeId", "regtime");
-        TeacherEntity teacherEntity = teacherRepository.getOne(teacherId);
-        List<ClassEntity> classlist = classRepository.findByTeacherEntity(teacherEntity);
-        List<NoticeEntity> noticelist = new ArrayList<>();
-        if(classId!=-1 && titleSearch!=null){
-            ClassEntity classentity = classRepository.getOne(classId);
-            noticelist = noticeRepository.findByClassEntityAndTitleContaining(classentity, titleSearch, sort);
-        } else if(classId==-1 && titleSearch!=null){
-            noticelist = noticeRepository.findByTitleContaining(titleSearch, sort);
-        } else if(classId!=-1 && titleSearch==null){
-            ClassEntity classentity = classRepository.getOne(classId);
-            noticelist = noticeRepository.findByClassEntity(classentity, sort);
-        } else {
-            noticelist = noticeRepository.findAll(sort);
-        }
-
-        List<NoticeEntity> result = new ArrayList<>();
-        for(ClassEntity classentity : classlist) {
-            for (NoticeEntity noticeentity : noticelist) {
-                if (classentity.equals(noticeentity.getClassEntity())) {
-                    result.add(noticeentity);
-                }
-            }
-        }
-        return result.stream().map(NoticeResponse::new).collect(Collectors.toList());
+        List<NoticeEntity> list = noticeRepository.findAll(sort);
+        return list.stream().map(NoticeResponse::new).collect(Collectors.toList());
     }
 
-    //공지사항 리스트 조회 및 검색(관리자)
-    public List<NoticeResponse> findAll(final int classId, final String titleSearch){
-        Sort sort = Sort.by(Sort.Direction.DESC, "noticeId", "regtime");
-        List<NoticeEntity> result = new ArrayList<>();
-        if(classId!=-1 && titleSearch!=null){
-            ClassEntity classentity = classRepository.getOne(classId);
-            result = noticeRepository.findByClassEntityAndTitleContaining(classentity, titleSearch, sort);
-        } else if(classId==-1 && titleSearch!=null){
-            result = noticeRepository.findByTitleContaining(titleSearch, sort);
-        } else if(classId!=-1 && titleSearch==null){
-            ClassEntity classentity = classRepository.getOne(classId);
-            result = noticeRepository.findByClassEntity(classentity, sort);
-        } else {
-            result = noticeRepository.findAll(sort);
-        }
-        return result.stream().map(NoticeResponse::new).collect(Collectors.toList());
-    }
-    //공지사항 페이지네이션
-
-    //공지사항 수정
-    @Transactional
-    public int update(final int noticeId, final NoticeRequest request){
-        NoticeEntity entity = noticeRepository.findById(noticeId).orElseThrow(() -> new CustomException(ErrorCode.POSTS_NOT_FOUND));
-        ClassEntity classEntity = classRepository.getOne(request.getClassId());
-        entity.update(classEntity, request.getTitle(), request.getContent());
-        return noticeId;
-    }
-
-    //공지사항 삭제
-    @Transactional
-    public void delete(final int noticeId){
-        NoticeEntity entity = noticeRepository.findById(noticeId).orElseThrow(() -> new CustomException(ErrorCode.POSTS_NOT_FOUND));
-        noticeRepository.delete(entity);
-    }
+    //게시글 수정
 }
