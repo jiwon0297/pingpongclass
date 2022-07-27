38de82a73c57a74077827d351fea49d23239aae7
jiwon0297 <https://github.com/jiwon0297>
2022-07-27 12:54:25 +0900

    :sparkles: 선생님, 학생 등록 시 임시비밀번호 발급 및 인코딩하여 DB 저장

---
+++ backend/src/main/java/com/pingpong/backend/config/SecurityConfig.java
@@ -0,0 +1,40 @@
+package com.pingpong.backend.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity // 시큐리티 활성화 -> 기본 스프링 필터체인에 등록
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+	@Autowired
+	private CorsConfig corsConfig;
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+				.addFilter(corsConfig.corsFilter())
+				.csrf().disable()
+				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			.and()
+				.formLogin().disable()
+				.httpBasic().disable();
+	}
+
+	@Bean
+	public BCryptPasswordEncoder encodePassword() {
+		return new BCryptPasswordEncoder();
+	}
+}
+
+
+
+
+
+
