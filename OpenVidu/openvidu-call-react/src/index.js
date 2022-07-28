b7387d014f58e38239870793689be30b7b1f18e9
Ssoco <dlscjs8646@gmail.com>
2022-07-29 01:33:17 +0900

    :recycle: 로딩처리 혹은 url처리를 위해서 필요할 수 있는 App.js 생성

--- OpenVidu/openvidu-call-react/src/index.js
+++ OpenVidu/openvidu-call-react/src/index.js
@@ -1,11 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import VideoRoomComponent from './components/VideoRoomComponent';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
 
 // HTML파일에서 root를 찾아서 그 곳에 VideoRoomComponent를 삽입
-ReactDOM.render(
-  <VideoRoomComponent />, document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
