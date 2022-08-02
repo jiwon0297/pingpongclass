19e3dd0c14a8fd6671ea4713fe249bd9d811ac68
Ssoco <dlscjs8646@gmail.com>
2022-08-02 12:32:18 +0900

    :sparkles: IosModal 컴포넌트 완성
    children,
      isOpen = true,
      renderCenter = false,
      isRelative = true,
      relativePos = {},
      absolutePos = {},
      width,
      height,
    
    위와 같은 매개변수를 통해서 편하게 Modal을 생성할 수 있게 됨!

--- client/src/components/Common/IosModalBase.jsx
+++ client/src/components/Common/IosModalBase.jsx
@@ -1,13 +1,22 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
 import PropTypes from 'prop-types';
+import RedEllipse from '../../assets/images/redEllipse.png';
+import YellowEllipse from '../../assets/images/yellowEllipse.png';
+import GreenEllipse from '../../assets/images/greenEllipse.png';
 
-const IosModalBase = ({ width, height }) => {
-  console.log('여기', width, height);
+const IosModalBase = ({ children, width, height }) => {
   return (
     <div css={totalContainer({ width, height })}>
-      <div css={YellowBackground({ width, height })} />
-      <div css={WhiteBackground({ width, height })} />
+      <div css={YellowBackground} />
+      <div css={WhiteBackground}>
+        <div css={Header}>
+          <img src={RedEllipse} alt="빨간버튼" />
+          <img src={YellowEllipse} alt="노란버튼" />
+          <img src={GreenEllipse} alt="초록버튼" />
+        </div>
+        <div css={Body}>{children}</div>
+      </div>
     </div>
   );
 };
@@ -17,7 +26,7 @@ const totalContainer = ({ width, height }) => css`
   height: ${height};
 `;
 
-const YellowBackground = () => css`
+const YellowBackground = css`
   position: absolute;
   width: 100%;
   height: 100%;
@@ -26,23 +35,43 @@ const YellowBackground = () => css`
   border-radius: 30px;
 `;
 
-const WhiteBackground = () => css`
+const WhiteBackground = css`
   position: absolute;
-  width: 99%;
-  height: 99%;
+  width: 99.5%;
+  height: 98%;
   left: 2%;
-  top: 1%;
   background: #ffffff;
   border: 5px solid #000000;
   border-radius: 20px;
 `;
 
+const Header = css`
+  width: 100.1%;
+  height: 3rem;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  border-bottom: 5px solid #000000;
+
+  img {
+    width: 1.5rem;
+    margin-left: 0.5rem;
+  }
+`;
+
+const Body = css`
+  width: 100%;
+  height: 100% - 3rem;
+`;
+
 IosModalBase.propTypes = {
+  children: PropTypes.node,
   width: PropTypes.string,
   height: PropTypes.string,
 };
 
 IosModalBase.defaultProps = {
+  children: null,
   width: '1000px',
   height: '600px',
 };
