dcf51f31b16aa3861f5c6b8167d08480d2bba2cf
Ssoco <dlscjs8646@gmail.com>
2022-08-02 14:32:47 +0900

    :bug: 머지처리

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import DashBoard from './pages/DashBoard';
@@ -15,17 +14,3 @@ function App() {
 }
 
 export default App;
-=======
-import './App.css';
-import Home from './pages/Home';
-
-function App() {
-  return (
-    <div className="App">
-      <Home />
-    </div>
-  );
-}
-
-export default App;
->>>>>>> 566e86fc1ba92ae3e4f9c221216d8249d721dbab
