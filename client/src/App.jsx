76c8ae83dd1caa90c9d7d0766eaae82aaff7bc65
jhwon07 <dnjswogh07@gmail.com>
2022-08-02 13:12:50 +0900

    :sparkles: Router 세팅

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,15 @@
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './views/Home';
+import DashBoard from './views/DashBoard';
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<DashBoard />} />
+      </Routes>
+    </Router>
   );
 }
 
