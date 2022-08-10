efa8c9ea8152e60b1af9c4f9bf90b570bc5e7232
DESKTOP-KVCQHCD\SSAFY <pyjin805@naver.com>
2022-08-11 04:12:11 +0900

    :bug: 중복함수 제거

--- backend/src/main/java/com/pingpong/backend/api/service/ClassService.java
+++ backend/src/main/java/com/pingpong/backend/api/service/ClassService.java
@@ -198,33 +198,6 @@ public class ClassService {
         classEntity.updateUrl(openRequest.getClassUrl());
     }
 
-    //시간표 출력용 요일별 수업 리스트 전체
-    public List<List<ClassResponse>> makeTimeTable(final int userId){
-        List<List<ClassResponse>> res = new ArrayList<>();
-        Sort sort = Sort.by(Sort.Direction.DESC,"TimetableEntity");
-        for(int i=1; i<6; i++){
-            List<ClassEntity> classEntityList = new ArrayList<>();
-            if(userId>1000000000) {// 학생일때
-                StudentEntity studentEntity = studentRepository.getOne(userId);
-                List<ClassStudentEntity> classStudentEntityList = classStudentRepository.findByStudentEntity(studentEntity);
-                for (ClassStudentEntity classStudentEntity : classStudentEntityList) {
-                    List<ClassEntity> subList = classRepository.findByClassIdAndClassDay(classStudentEntity.getClassEntity().getClassId(), i, sort);
-                    for(ClassEntity classEntity:subList)
-                        classEntityList.add(classEntity);
-                }
-            }else { //선생님일때
-                TeacherEntity teacherEntity = teacherRepository.findByTeacherId(userId);
-                List<ClassEntity> subList = classRepository.findByTeacherEntityAndClassDay(teacherEntity, i, sort);
-                for(ClassEntity classEntity:subList)
-                    classEntityList.add(classEntity);
-            }
-            List<ClassResponse> list = classEntityList.stream().map(ClassResponse::new).collect(Collectors.toList());
-            res.add(list);
-        }
-        return res;
-    }
-
-
     @Transactional
     public boolean isOpen(int classId){
         String classUrl = classRepository.getOne(classId).getClassUrl();
