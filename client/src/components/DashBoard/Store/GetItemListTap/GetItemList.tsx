ffa639f43f26828b4ddfba33b15afab8c255f483
DESKTOP-KVCQHCD\SSAFY <pyjin805@naver.com>
2022-08-13 02:54:40 +0900

    :sparkles: 아이템 뽑기 API 완성, 애니메이션 틀, 희귀도 겹침 이슈 해결

--- client/src/components/DashBoard/Store/GetItemListTap/GetItemList.tsx
+++ client/src/components/DashBoard/Store/GetItemListTap/GetItemList.tsx
@@ -1,6 +1,10 @@
 import { css } from '@emotion/react';
 import { useState, useCallback, useEffect } from 'react';
 import { useAppSelector, useAppDispatch } from '@src/store/hooks';
+import { toast } from 'react-toastify';
+import axios from 'axios';
+import { setupInterceptorsTo } from '@utils/AxiosInterceptor';
+
 import member, {
   setPoint,
   saveMember,
@@ -9,24 +13,18 @@ import member, {
   saveItem,
 } from '@src/store/member';
 
-function Item({ item }) {
-  const img = '/items/' + item.itemId + '.png';
-  return (
-    <div className="colorchangeBorder">
-      <img src={img} style={{ width: '80%' }} />
-      <p>
-        {item.name}:{item.cnt}
-      </p>
-    </div>
-  );
+interface GetItemListProps {
+  highFunction: Function;
 }
 
-const GetItemList = () => {
+const GetItemList = (props: GetItemListProps) => {
   // const { items } = props;
   const [items, setItems] = useState<Items[]>([allItems]);
   const [loading, setLoading] = useState(true);
   const memberStore = useAppSelector((state) => state.member);
   const dispatch = useAppDispatch();
+  const InterceptedAxios = setupInterceptorsTo(axios.create());
+  const [border, setBorder] = useState(props);
 
   useEffect(() => {
     //로딩시 해당 유저의 아이템 불러오기
@@ -37,6 +35,79 @@ const GetItemList = () => {
       setLoading(false);
     });
   }, []);
+
+  const onClickItem = (e) => {
+    if (e.itemId > 2) {
+      toast.warn('해당 아이템은 수업 중 사용이 가능합니다.');
+    } else if (confirm(e.name + '을 사용하시겠습니까?')) {
+      //색상 랜덤 뽑기
+      //색상 1, 2-3, 4-8
+      //색상 확률 1, 9, 90
+      let color = 0;
+      const rarity = Math.floor(Math.random() * 100) + 1; //1~100까지
+      if (rarity >= 10) {
+        color = Math.floor(Math.random() * 5) + 4;
+      } else if (rarity >= 2) {
+        color = Math.floor(Math.random() * 2) + 2;
+      } else {
+        color = 1;
+      }
+
+      InterceptedAxios.delete(`/items/${memberStore.userId}/${e.itemId}`)
+        .then(() => {
+          //테두리
+          if (e.itemId === 1) {
+            InterceptedAxios.patch('/items/color/border', {
+              studentId: memberStore.userId,
+              color: color,
+            })
+              .then(() => {
+                props.highFunction(color, 1);
+              })
+              .catch(function (error) {
+                toast.warn('테두리 컬러뽑기 중 에러 발생');
+                console.log(error);
+              });
+          } else {
+            //퐁퐁이 참여도
+            InterceptedAxios.patch('/items/color/jandi', {
+              studentId: memberStore.userId,
+              color: color,
+            })
+              .then(() => {
+                props.highFunction(color, 2);
+              })
+              .catch(function (error) {
+                toast.warn('퐁퐁이 컬러뽑기 중 에러 발생');
+                console.log(error);
+              });
+          }
+        })
+        .catch(function (error) {
+          toast.warn('아이템 사용 중 에러 발생');
+          console.log(error);
+        });
+    }
+
+    console.log(e);
+  };
+
+  function Item({ item }) {
+    const img = '/items/' + item.itemId + '.png';
+    return (
+      <div
+        className="colorchangeBorder"
+        onClick={(e) => {
+          onClickItem(item);
+        }}
+      >
+        <img src={img} style={{ width: '80%' }} />
+        <p>
+          {item.name}:{item.cnt}
+        </p>
+      </div>
+    );
+  }
 
   // console.log(items, ',,');
   const filterItem = items.filter(
