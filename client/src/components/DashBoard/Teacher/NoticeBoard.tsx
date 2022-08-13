0db4b1b9dad2285f323f31158d5c32662560e512
jiwon0297 <https://github.com/jiwon0297>
2022-08-13 22:56:06 +0900

    :sparkles: notice 글 상세 조회 api 작성

--- client/src/components/DashBoard/Teacher/NoticeBoard.tsx
+++ client/src/components/DashBoard/Teacher/NoticeBoard.tsx
@@ -182,7 +182,8 @@ const NoticeBoard = () => {
   return (
     <div css={NoticeBoardStyle}>
       <div className="upperModalArea">
-        <div className="pageTitle">공지사항(선생님)</div>
+        <div className="pageTitle">공지사항</div>
+        <hr />
         <form onSubmit={search} className="search-div">
           <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
             <InputLabel id="demo-simple-select-label">수업명</InputLabel>
