39d33769ad3310fc2ecd089181479839c638e95f
jiwon0297 <https://github.com/jiwon0297>
2022-08-14 17:57:14 +0900

    :lipstick: 수업 통계 화면 (선생님, 학생) CSS 디자인 완료

--- client/src/openvidu/components/result/TeacherResult.jsx
+++ client/src/openvidu/components/result/TeacherResult.jsx
@@ -59,8 +59,8 @@ const TeacherResult = ({
   // 스크롤 처리 해야함
 
   const data = [
-    { name: '참여 학생', value: studentList.length },
-    { name: '미참여 학생', value: attStudentNum },
+    { name: '참여 학생', value: attStudentNum },
+    { name: '미참여 학생', value: studentList.length - attStudentNum },
   ];
 
   const renderCustomizedLabel = ({
@@ -105,113 +105,273 @@ const TeacherResult = ({
   };
 
   const COLORS = [
-    '#febe00',
-    '#577fdc',
-    '#fe5500',
-    '#1c8044',
-    '#742a91',
-    '#31aaa6',
+    '#81a5f9',
+    '#f3ab7e',
+    '#6cb07d',
+    '#7f67a1',
+    '#71c4c1',
+    '#ecd691',
   ];
 
   return (
-    <div css={TotalContainer}>
-      <h2>고생하셨습니다 {teacherName} 선생님!</h2>
-      <br />
-      <h3 style={{ margin: '0' }}>수업 통계</h3>
-      <div css={ClassStatistic}>
-        <div className="teacherResult">
-          <div className="teacher index">
-            <div className="t-classname">수업 이름</div>
-            <div className="t-nickname">선생님 이름</div>
-            <div className="t-attendence-time">수업 개설 시간</div>
-            <div className="t-fin-time">수업 종료 시간</div>
-            <div className="t-point">총 부여 상점</div>
-          </div>
-          <div className="teacher">
-            <div className="t-classname">{classTitle}</div>
-            <div className="t-nickname">{teacherName}</div>
-            <div className="t-attendence-time">{myData.attendenceTime}</div>
-            <div className="t-fin-time">{finTime}</div>
-            <div className="t-point">{totalSticker}</div>
-          </div>
-        </div>
-        <div>
-          <PieChart width={400} height={400}>
-            <Pie
-              label={renderCustomizedLabel}
-              data={data}
-              cx="50%"
-              cy="50%"
-              innerRadius={60}
-              outerRadius={80}
-              fill="#8884d8"
-              dataKey="value"
-            >
-              {data.map((entry, index) => (
-                <Cell
-                  key={`cell-${index}`}
-                  fill={COLORS[index % COLORS.length]}
-                />
-              ))}
-            </Pie>
-          </PieChart>
-        </div>
+    <div css={BackgroundContainer}>
+      <div className="triangles">
+        <div className="triangle1" />
+        <div className="triangle2" />
       </div>
-      <h3>학생 통계</h3>
-      <div css={TotalResult}>
-        <div className="studentResult">
-          <div className="person index">
-            <div className="s-nickname">닉네임</div>
-            <div className="s-attendence-time">최종 출석 시간</div>
-            <div className="s-point">상점</div>
-          </div>
-          {othersData.map((other, i) => (
-            <div key={i} className="person">
-              <div className="s-nickname">{other.nickname}</div>
-              <div className="s-attendence-time">{other.attendenceTime}</div>
-              <div className="s-point">{other.point}</div>
+      <div className="parent">
+        <div className="child">
+          <div className="container">
+            <div className="circles">
+              <div className="circle1" />
+              <div className="circle2" />
+              <div className="circle3" />
             </div>
-          ))}
-        </div>
-        <div>
-          <BarChart
-            width={500}
-            height={300}
-            data={othersData}
-            margin={{
-              top: 5,
-              right: 30,
-              left: 20,
-              bottom: 5,
-            }}
-          >
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="nickname" />
-            <YAxis />
-            <Tooltip />
-            <Legend />
-            <Bar dataKey="point" fill="#8884d8" label={{ position: 'top' }}>
-              {othersData.map((entry, index) => (
-                <Cell key={`cell-${index}`} fill={COLORS[index % 20]} />
-              ))}
-            </Bar>
-          </BarChart>
-        </div>
-      </div>
-      <div css={OtherThings}>
-        <Link to={`/teacher`}>
-          <div className="btn-items">
-            <button onClick={applyToDB}>
-              로그 저장 후 대시보드로 돌아가기
-            </button>
+            <hr />
+            <div className="sideContainer">
+              <div css={TotalContainer}>
+                <h2>고생하셨습니다 {teacherName} 선생님!</h2>
+                <br />
+                <div css={ClassStatistic}>
+                  <h3 style={{ margin: '0' }}>수업 통계</h3>
+                  <div className="classContainer">
+                    <div className="teacherResult">
+                      <div className="teacher index">
+                        <div className="t-classname">수업 이름</div>
+                        <div className="t-nickname">선생님 이름</div>
+                        <div className="t-attendence-time">수업 개설 시간</div>
+                        <div className="t-fin-time">수업 종료 시간</div>
+                        <div className="t-point">총 부여 상점</div>
+                      </div>
+                      <div className="teacher">
+                        <div className="t-classname">{classTitle}</div>
+                        <div className="t-nickname">{teacherName}</div>
+                        <div className="t-attendence-time">
+                          {myData.attendenceTime}
+                        </div>
+                        <div className="t-fin-time">{finTime}</div>
+                        <div className="t-point">{totalSticker}</div>
+                      </div>
+                    </div>
+                    <div className="teacherResultChart">
+                      <PieChart width={490} height={200}>
+                        <Pie
+                          label={renderCustomizedLabel}
+                          data={data}
+                          cx="50%"
+                          cy="50%"
+                          innerRadius={60}
+                          outerRadius={80}
+                          fill="#8884d8"
+                          dataKey="value"
+                        >
+                          {data.map((entry, index) => (
+                            <Cell
+                              key={`cell-${index}`}
+                              fill={COLORS[index % COLORS.length]}
+                            />
+                          ))}
+                        </Pie>
+                      </PieChart>
+                    </div>
+                  </div>
+                </div>
+                <div css={TotalResult}>
+                  <h3 style={{ margin: '0' }}>학생 통계</h3>
+                  <div className="studentContainer">
+                    <div className="studentResult">
+                      <div className="person index">
+                        <div className="s-nickname">닉네임</div>
+                        <div className="s-attendence-time">최종 출석 시간</div>
+                        <div className="s-point">상점</div>
+                      </div>
+                      {othersData.map((other, i) => (
+                        <div key={i} className="person">
+                          <div className="s-nickname">{other.nickname}</div>
+                          <div className="s-attendence-time">
+                            {other.attendenceTime}
+                          </div>
+                          <div className="s-point">{other.point}</div>
+                        </div>
+                      ))}
+                    </div>
+                    <div>
+                      <BarChart
+                        width={450}
+                        height={270}
+                        data={othersData}
+                        margin={{
+                          top: 24,
+                        }}
+                      >
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="nickname" style={{ fontSize: '9pt' }} />
+                        <YAxis />
+                        <Tooltip />
+                        <Legend />
+                        <Bar
+                          dataKey="point"
+                          fill="#333"
+                          label={{ position: 'top' }}
+                          background={{ fill: '#eee' }}
+                        >
+                          {othersData.map((entry, index) => (
+                            <Cell
+                              key={`cell-${index}`}
+                              fill={COLORS[index % 20]}
+                            />
+                          ))}
+                        </Bar>
+                      </BarChart>
+                    </div>
+                  </div>
+                </div>
+                <div css={OtherThings}>
+                  <Link to={`/teacher`}>
+                    <button onClick={applyToDB}>저장 후 돌아가기</button>
+                  </Link>
+                </div>
+              </div>
+            </div>
           </div>
-        </Link>
+        </div>
       </div>
     </div>
   );
 };
 
+const BackgroundContainer = css`
+  background-color: #ffffff;
+  opacity: 0.8;
+  background-image: linear-gradient(#929292 1px, transparent 1px),
+    linear-gradient(to right, #929292 1px, #ffffff 1px);
+  background-size: 30px 30px;
+  height: 100vh;
+  position: relative;
+
+  button {
+    width: 200px;
+    height: 40px;
+    border-radius: 20px;
+    background: var(--pink);
+    border: none;
+    font-family: 'NanumSquareRound';
+    font-size: 13pt;
+    color: white;
+    font-weight: 700;
+    text-decoration-line: none;
+    margin-top: 15px;
+  }
+
+  .triangles {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+
+    div {
+      width: 0px;
+      height: 0px;
+    }
+
+    .triangle1 {
+      border-bottom: 40vh solid #bdcde5;
+      border-left: 0px solid transparent;
+      border-right: 100vw solid transparent;
+      transform: scaleY(-1);
+    }
+
+    .triangle2 {
+      position: absolute;
+      top: 60%;
+      border-bottom: 40vh solid #f8cbd3;
+      border-left: 100vw solid transparent;
+      border-right: 0px solid transparent;
+    }
+  }
+
+  .child {
+    position: absolute;
+    width: 79.8vw;
+    height: 83vh;
+    left: 0.5vw;
+    top: -0.5vh;
+
+    border-radius: 20px;
+
+    background-color: white;
+
+    border: 2px solid #000000;
+  }
+
+  .parent {
+    position: relative;
+    width: 80vw;
+    height: 84vh;
+    left: 9.8vw;
+    top: 8vh;
+
+    border-radius: 20px;
+
+    background-color: #fff1bf;
+
+    border: 2px solid #001111;
+  }
+
+  .container {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    justify-content: start;
+  }
+
+  .sideContainer {
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    height: 78vh;
+    vertical-align: center;
+  }
+
+  hr {
+    width: 100%;
+    height: 2px;
+    border: 0;
+    background-color: black;
+  }
+
+  .circles {
+    display: flex;
+    flex-direction: row;
+    justify-content: end;
+    margin: 10px 10px 0 10px;
+    div {
+      border: 2px solid black;
+      width: 10px;
+      height: 10px;
+      border-radius: 75px;
+      background-color: #000000;
+      float: left;
+    }
+
+    .circle1 {
+      background-color: #96ba85;
+    }
+
+    .circle2 {
+      background-color: #ffe381;
+      margin-left: 0.3rem;
+    }
+
+    .circle3 {
+      background-color: #ef8181;
+      margin-left: 0.3rem;
+    }
+  }
+`;
+
 const TotalContainer = css`
+  width: 100%;
+  height: 100%;
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -219,14 +379,28 @@ const TotalContainer = css`
 `;
 
 const ClassStatistic = css`
+  width: 70%;
+  height: 35%;
+  border-radius: 20px;
+  margin-top: 10px;
+  align-items: center;
   display: flex;
-  flex-direction: row;
-  margin-top: 20px;
+  flex-direction: column;
+  border: 1px solid lightgray;
+  justify-content: center;
 
+  .classContainer {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+    align-items: center;
+  }
   .teacherResult {
     display: flex;
     flex-direction: row;
-    margin-top: 5rem;
+    background: #fff7d9;
+    margin-right: 20px;
+    border-radius: 5px;
   }
 
   h3 {
@@ -239,8 +413,8 @@ const ClassStatistic = css`
 
     & > div {
       display: flex;
-      margin: 1rem;
-      height: 1rem;
+      margin: 0.7rem;
+      height: 0.5rem;
       align-items: center;
       justify-content: center;
       text-align: center;
@@ -277,16 +451,38 @@ const ClassStatistic = css`
       align-items: center;
     }
   }
+
+  .index {
+    font-weight: 700;
+  }
 `;
 
 const TotalResult = css`
+  width: 70%;
+  height: 45%;
+  border-radius: 20px;
+  margin-top: 10px;
+  align-items: center;
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
+  border: 1px solid lightgray;
+  justify-content: center;
+
+  .studentContainer {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+  }
 
   .studentResult {
     display: flex;
     flex-direction: column;
-    margin: 2rem;
+    width: 40%;
+    height: 80%;
+    background: #fff7d9;
+    margin-right: 20px;
+    border-radius: 5px;
   }
 
   h3 {
@@ -299,8 +495,8 @@ const TotalResult = css`
 
     & > div {
       display: flex;
-      margin: 1rem;
-      height: 1rem;
+      margin: 0.7rem;
+      height: 0.5rem;
       align-items: center;
       justify-content: center;
       text-align: center;
@@ -326,6 +522,10 @@ const TotalResult = css`
       justify-content: center;
       align-items: center;
     }
+  }
+
+  .index {
+    font-weight: 700;
   }
 `;
 
