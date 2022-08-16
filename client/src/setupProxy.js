db0be9a8a70e28f5ca98416fc10378cea50674f9
김민지 <whiterisi@naver.com>
2022-08-17 00:19:16 +0900

    'https://i7a403.p.ssafy.io',

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     '/be',
     createProxyMiddleware({
-      target: 'http://i7a403.p.ssafy.io:8080',
+      target: 'https://i7a403.p.ssafy.io',
       changeOrigin: true,
     }),
   );
